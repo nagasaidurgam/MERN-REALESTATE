1cb90d4c50faf4cfd7cfea6e47ff67ad77fedca9
Durga <nagasaidurgam@gmail.com>
2026-02-27 15:29:47 +0530

    Create Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import  About from './pages/About';
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>
-     App
-    </h1>
+    <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/Profile' element={<Profile />} />
+     </Routes>
+    </BrowserRouter>
   )
 }
+//we need to import a browser router, routes, route, 
+//everything is covered with the browser router and routes
+//ALT+Shift+down arrow copy route lines
+//in routes creating signin, signup import on top that route
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,3 +16,5 @@ export default {
 in terminal npm create vite@latest client react js+swc no yes cd client npm i npm install, npm install -D, 
 npx tailwindcss init -package, npm run dev */
 //delete App.css  because we are using tailwind css., delete vite.svg, react .jsv, 
+//github repositary - git init, git add ., git .commit -m, git commit -m "install react js and tailwindcss and create the first template", 
+//git remote add origin gitlink, git branch -m main, git push -u origin main
