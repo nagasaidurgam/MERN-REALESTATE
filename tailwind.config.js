adb64e73a91f5102623b110b2f293ddcd987ba3c
Durga <nagasaidurgam@gmail.com>
2026-02-27 14:26:12 +0530

    install react js and tailwindcss and create the first template

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+/* 
+//firstly intalling react and tailwind css
+in terminal npm create vite@latest client react js+swc no yes cd client npm i npm install, npm install -D, 
+npx tailwindcss init -package, npm run dev */
+//delete App.css  because we are using tailwind css., delete vite.svg, react .jsv, 
