365f828b3a97088a48de0e7b579e3e72e2e045b7
Durga <nagasaidurgam@gmail.com>
2026-03-02 11:12:49 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import  About from './pages/About';
+import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/header';
 
 export default function App() {
   return (
     <BrowserRouter>
-     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='sign-in' element={<SignIn />} />
-      <Route path='/sign-up' element={<SignUp />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/Profile' element={<Profile />} />
-     </Routes>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
+
 //we need to import a browser router, routes, route, 
 //everything is covered with the browser router and routes
 //ALT+Shift+down arrow copy route lines
