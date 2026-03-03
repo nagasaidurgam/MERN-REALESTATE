418644809d8be58790f22c0133a65fc5ffe91458
Durga <nagasaidurgam@gmail.com>
2026-03-03 10:42:25 +0530

    Create a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,8 @@
+export const test = (req, res)=> {
+    res.json({
+        message: 'Api route is working!',
+    });
+}
+
+
+//app.get ('/' req - client side, res- server side response for creating )
--- api/index.js
+++ api/index.js
@@ -1,17 +1,28 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'; //cant use an environmental variable inside the backend you need to install dotenv
-dotenv.config()
-mongoose.connect(process.env.MONGO).then(()=> {
-    console.log('Connected to MongoDB!'); //for feedback purpose using then catch
-}).catch((err) => {
+
+import userRouter from './routes/user.route.js';
+
+dotenv.config() 
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(()=> {
+        console.log('Connected to MongoDB!'); //for feedback purpose using then catch
+    })
+    .catch((err) => {
     console.log(err);
 });
 const app = express();
+
+
+
 app.listen(3000, ()=> {
     console.log("Server is running on port 3000");
 });
 
+app.use('/api/user', userRouter);
 
 
 //create and run the server create api folder package.json file should be inside the root.
